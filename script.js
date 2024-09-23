bdcbe7f45ac063dd3f206218552b0e3a7163b01b
Ferdous <161162257+NayokCoder@users.noreply.github.com>
2024-09-23 16:40:25 +0600

    js
    js

--- script.js
+++ script.js
@@ -13,9 +13,27 @@ document
   .getElementById("nowakhali-btn")
   .addEventListener("click", function (even) {
     even.preventDefault();
+
     const nowaKhali_Input = getInputValue("nowakhali-input");
     const nowaKhalDonation_Amount = getInputText("noakhali-total-donation");
+    const myAmount = getInputText("my-amount");
     const totalDonation = nowaKhalDonation_Amount + nowaKhali_Input;
-    console.log(totalDonation);
-    console.log(nowaKhalDonation_Amount);
+    const myRemainAmount = myAmount - nowaKhali_Input;
+    document.getElementById("noakhali-total-donation").innerText =
+      totalDonation;
+
+    document.getElementById("my-amount").innerText = myRemainAmount;
   });
+
+document.getElementById("feni-btn").addEventListener("click", function (even) {
+  even.preventDefault();
+
+  const feni_Input = getInputValue("feni-input");
+  const feniDonation_Amount = getInputText("feni-total-donation");
+  const myAmount = getInputText("my-amount");
+  const totalDonation = feniDonation_Amount + feni_Input;
+  const myRemainAmount = myAmount - feni_Input;
+  document.getElementById("feni-total-donation").innerText = totalDonation;
+
+  document.getElementById("my-amount").innerText = myRemainAmount;
+});
