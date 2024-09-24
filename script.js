5c2a8540632d2d69e2f5eb15d39df57bce51d0bd
Ferdous <ferdous.anon@gmail.com>
2024-09-24 16:01:28 +0600

    validation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getInputValue(id) {
   return parseFloat(document.getElementById(id).value);
 }
@@ -10,6 +9,7 @@ document.getElementById("blog").addEventListener("click", function () {
   window.location.href = "blog.html";
 });
 
+//? NOWAKHALI CLICK EVENT
 document
   .getElementById("nowakhali-btn")
   .addEventListener("click", function (even) {
@@ -24,14 +24,27 @@ document
       totalDonation;
 
     document.getElementById("my-amount").innerText = myRemainAmount;
-    const donateHeading = document.getElementById("nh2");
+
+    const donateHeading = document.getElementById("nh2").innerText;
     const history = document.getElementById("history");
     const creatDiv = document.createElement("div");
     const createHeading = document.createElement("h1");
     const createPara = document.createElement("p");
 
-    createHeading.innerText = `${nowaKhalDonation_Amount} TK is ${donateHeading} `;
-    console.log(createHeading);
+    createHeading.innerText = `${nowaKhali_Input} TK is ${donateHeading} `;
+
+    // CSS Property
+    creatDiv.classList.add(
+      "p-8",
+      "bg-base-100",
+      "rounded-lg",
+      "border-solid",
+      "border-2",
+      "border-gray-100",
+      "mt-4"
+    );
+    createHeading.classList.add("font-bold", "text-xl");
+
     const currentDate = new Date();
     const date = currentDate.toLocaleDateString();
     const time = currentDate.toLocaleTimeString();
@@ -41,6 +54,7 @@ document
     history.appendChild(creatDiv);
   });
 
+//? feni CLICK EVENT
 document.getElementById("feni-btn").addEventListener("click", function (even) {
   even.preventDefault();
 
@@ -52,60 +66,97 @@ document.getElementById("feni-btn").addEventListener("click", function (even) {
   document.getElementById("feni-total-donation").innerText = totalDonation;
 
   document.getElementById("my-amount").innerText = myRemainAmount;
+
+  const donateHeading = document.getElementById("fh2").innerText;
+  const history = document.getElementById("history");
+  const creatDiv = document.createElement("div");
+  const createHeading = document.createElement("h1");
+  const createPara = document.createElement("p");
+
+  createHeading.innerText = `${feni_Input} TK is ${donateHeading} `;
+
+  // CSS Property
+  creatDiv.classList.add(
+    "p-8",
+    "bg-base-100",
+    "rounded-lg",
+    "border-solid",
+    "border-2",
+    "border-gray-100",
+    "mt-4"
+  );
+  createHeading.classList.add("font-bold", "text-xl");
+
+  const currentDate = new Date();
+  const date = currentDate.toLocaleDateString();
+  const time = currentDate.toLocaleTimeString();
+  createPara.innerText = `Donation made on: ${date} at ${time}`;
+  creatDiv.appendChild(createHeading);
+  creatDiv.appendChild(createPara);
+  history.appendChild(creatDiv);
 });
 
+//? quota CLICK EVENT
 document.getElementById("quota-btn").addEventListener("click", function (even) {
   even.preventDefault();
 
   const Quota_Input = getInputValue("quota-input");
-  console.log(Quota_Input);
+
   const QuotaDonation_Amount = getInputText("quota-total-donation");
-  console.log(QuotaDonation_Amount);
+
   const myAmount = getInputText("my-amount");
   const totalDonation = QuotaDonation_Amount + Quota_Input;
   const myRemainAmount = myAmount - Quota_Input;
   document.getElementById("quota-total-donation").innerText = totalDonation;
 
   document.getElementById("my-amount").innerText = myRemainAmount;
-});
-=======
-function getInputValue(id) {
-  return parseFloat(document.getElementById(id).value);
-}
-function getInputText(id) {
-  return parseFloat(document.getElementById(id).innerText);
-}
 
-document.getElementById("blog").addEventListener("click", function () {
-  window.location.href = "blog.html";
+  const donateHeading = document.getElementById("qh2").innerText;
+  const history = document.getElementById("history");
+  const creatDiv = document.createElement("div");
+  const createHeading = document.createElement("h1");
+  const createPara = document.createElement("p");
+
+  createHeading.innerText = `${Quota_Input} TK is ${donateHeading} `;
+
+  const currentDate = new Date();
+  const date = currentDate.toLocaleDateString();
+  const time = currentDate.toLocaleTimeString();
+  createPara.innerText = `Donation made on: ${date} at ${time}`;
+  // CSS Property
+  creatDiv.classList.add(
+    "p-8",
+    "bg-base-100",
+    "rounded-lg",
+    "border-solid",
+    "border-2",
+    "border-gray-100",
+    "mt-4"
+  );
+  createHeading.classList.add("font-bold", "text-xl");
+  creatDiv.appendChild(createHeading);
+  creatDiv.appendChild(createPara);
+  history.appendChild(creatDiv);
 });
 
+document.getElementById("donation").addEventListener("click", function (even) {
+  even.preventDefault();
+  document.getElementById("history").classList.add("hidden");
+  document.getElementById("donation-section").classList.remove("hidden");
+});
 document
-  .getElementById("nowakhali-btn")
+  .getElementById("history-home")
   .addEventListener("click", function (even) {
     even.preventDefault();
-
-    const nowaKhali_Input = getInputValue("nowakhali-input");
-    const nowaKhalDonation_Amount = getInputText("noakhali-total-donation");
-    const myAmount = getInputText("my-amount");
-    const totalDonation = nowaKhalDonation_Amount + nowaKhali_Input;
-    const myRemainAmount = myAmount - nowaKhali_Input;
-    document.getElementById("noakhali-total-donation").innerText =
-      totalDonation;
-
-    document.getElementById("my-amount").innerText = myRemainAmount;
+    document.getElementById("donation-section").classList.add("hidden");
+    document.getElementById("history").classList.remove("hidden");
   });
 
-document.getElementById("feni-btn").addEventListener("click", function (even) {
-  even.preventDefault();
+// VALIDATION
 
-  const feni_Input = getInputValue("feni-input");
-  const feniDonation_Amount = getInputText("feni-total-donation");
+function zeroBalance() {
   const myAmount = getInputText("my-amount");
-  const totalDonation = feniDonation_Amount + feni_Input;
-  const myRemainAmount = myAmount - feni_Input;
-  document.getElementById("feni-total-donation").innerText = totalDonation;
-
-  document.getElementById("my-amount").innerText = myRemainAmount;
-});
->>>>>>> bdcbe7f45ac063dd3f206218552b0e3a7163b01b
+  if (myAmount >= 0) {
+    alert("You Don'T Have Balance");
+  }
+}
