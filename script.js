3cad2aa852ccb141f1630b3acf03645158980975
Ferdous <ferdous.anon@gmail.com>
2024-09-24 12:02:48 +0600

    history

--- script.js
+++ script.js
@@ -13,9 +13,57 @@ document
   .getElementById("nowakhali-btn")
   .addEventListener("click", function (even) {
     even.preventDefault();
+
     const nowaKhali_Input = getInputValue("nowakhali-input");
     const nowaKhalDonation_Amount = getInputText("noakhali-total-donation");
+    const myAmount = getInputText("my-amount");
     const totalDonation = nowaKhalDonation_Amount + nowaKhali_Input;
-    console.log(totalDonation);
-    console.log(nowaKhalDonation_Amount);
+    const myRemainAmount = myAmount - nowaKhali_Input;
+    document.getElementById("noakhali-total-donation").innerText =
+      totalDonation;
+
+    document.getElementById("my-amount").innerText = myRemainAmount;
+    const donateHeading = document.getElementById("nh2");
+    const history = document.getElementById("history");
+    const creatDiv = document.createElement("div");
+    const createHeading = document.createElement("h1");
+    const createPara = document.createElement("p");
+
+    createHeading.innerText = `${nowaKhalDonation_Amount} TK is ${donateHeading} `;
+    console.log(createHeading);
+    const currentDate = new Date();
+    const date = currentDate.toLocaleDateString();
+    const time = currentDate.toLocaleTimeString();
+    createPara.innerText = `Donation made on: ${date} at ${time}`;
+    creatDiv.appendChild(createHeading);
+    creatDiv.appendChild(createPara);
+    history.appendChild(creatDiv);
   });
+
+document.getElementById("feni-btn").addEventListener("click", function (even) {
+  even.preventDefault();
+
+  const feni_Input = getInputValue("feni-input");
+  const feniDonation_Amount = getInputText("feni-total-donation");
+  const myAmount = getInputText("my-amount");
+  const totalDonation = feniDonation_Amount + feni_Input;
+  const myRemainAmount = myAmount - feni_Input;
+  document.getElementById("feni-total-donation").innerText = totalDonation;
+
+  document.getElementById("my-amount").innerText = myRemainAmount;
+});
+
+document.getElementById("quota-btn").addEventListener("click", function (even) {
+  even.preventDefault();
+
+  const Quota_Input = getInputValue("quota-input");
+  console.log(Quota_Input);
+  const QuotaDonation_Amount = getInputText("quota-total-donation");
+  console.log(QuotaDonation_Amount);
+  const myAmount = getInputText("my-amount");
+  const totalDonation = QuotaDonation_Amount + Quota_Input;
+  const myRemainAmount = myAmount - Quota_Input;
+  document.getElementById("quota-total-donation").innerText = totalDonation;
+
+  document.getElementById("my-amount").innerText = myRemainAmount;
+});
